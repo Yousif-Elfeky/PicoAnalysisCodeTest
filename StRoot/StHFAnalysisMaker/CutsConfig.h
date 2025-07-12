a1ba4c06d555171e6b9d4668b1c10e0964a1ba01
Yousif Shabaan El-feky <yousifjoo@aucegypt.edu>
2025-07-12 10:53:44 +0300

    Update CutsConfig.h

--- StRoot/StHFAnalysisMaker/CutsConfig.h
+++ StRoot/StHFAnalysisMaker/CutsConfig.h
@@ -10,8 +10,8 @@ class StPicoEvent;
 namespace HFCuts {
     // -------- Event cuts (common) --------
     namespace Event {
-        constexpr float VzMax      = 30.f;   // |Vz| (cm) for collider; ignore for FXT with special window below
-        constexpr float VzDiffMax  = 3.f;    // |Vz - VzVPD|
+        constexpr float VzMax      = 60.f;   // |Vz| (cm) for collider; ignore for FXT with special window below
+        constexpr float VzDiffMax  = 6.f;    // |Vz - VzVPD|
         // Fixed-target window along beamline (cm)
         constexpr float VzMinFXT   = 185.f;
         constexpr float VzMaxFXT   = 235.f;
@@ -19,10 +19,10 @@ namespace HFCuts {
 
     // -------- Track quality --------
     namespace Track {
-        constexpr int   nHitsFitMin    = 20;   // FXT: 15
+        constexpr int   nHitsFitMin    = 15;   // FXT: 15
         constexpr float nHitsFracMin   = 0.52;
-        constexpr float dcaMax         = 1.0;  // FXT: 3.0
-        constexpr float ptMin          = 0.2f; // FXT: 0.1
+        constexpr float dcaMax         = 3.0;  // FXT: 3.0
+        constexpr float ptMin          = 0.1f; // FXT: 0.1
     }
 
     // -------- PID windows --------
@@ -32,7 +32,7 @@ namespace HFCuts {
         constexpr float nSigmaPi  = 2.0;
         constexpr float nSigmaK   = 2.0;
         // TOF 1/beta cut (absolute)
-        constexpr float oneOverBetaMax = 0.03; // 19 GeV, loosen to 0.04 at FXT
+        constexpr float oneOverBetaMax = 0.05; // 19 GeV, loosen to 0.04 at FXT
         // E/p window for electrons (BEMC)
         constexpr float eopMin = 0.8;
         constexpr float eopMax = 1.2;
